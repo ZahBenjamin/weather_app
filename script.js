b8603226e5a98aae78775687c393ddbbe205a956
ZahBenjamin <bz@benzah.com>
2022-03-08 16:08:52 -0500

    Upgrade scaffolding

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const api_key = "673915a845190fbcf5deefda622274f1";
 // var issueContainer = $('#issues');
 var searchBtnEl = document.getElementById('searchBtn');
 
 function getApi() {
-  var requestUrl = '';
+  var requestUrl = 'api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + api_key;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -10,22 +11,24 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
-      for (var i = 0; i < data.length; i++) {
 
-        //1 Make a piece of HTML!!
-        var userName = $('<h3>')
-        var issueTitle = $('<p>');
+      // sample loop below
+      // for (var i = 0; i < data.length; i++) {
 
-        //2 DRESS that html up how you want it!!
-        userName.text(data[i].user.login)
-        issueTitle.text(data[i].title)
+      //   //1 Make a piece of HTML!!
+      //   var userName = $('<h3>')
+      //   var issueTitle = $('<p>');
+
+      //   //2 DRESS that html up how you want it!!
+      //   userName.text(data[i].user.login)
+      //   issueTitle.text(data[i].title)
         
 
-        // //3 Chuck that piece of html onto the page!!!
-        issueContainer.append(userName);
-        issueContainer.append(issueTitle);
+      //   // //3 Chuck that piece of html onto the page!!!
+      //   issueContainer.append(userName);
+      //   issueContainer.append(issueTitle);
 
-      }
+      // }
     });
 }
 fetchButton.addEventListener('click', getApi);
